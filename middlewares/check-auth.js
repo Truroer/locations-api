66ad4f6a490dd6596387066d60829290497e45fc
Maxim Sinelnikov <maxim@sinelnikov.info>
2019-05-06 02:45:47 +0200

    cleanup

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -4,7 +4,6 @@ const User = require("../models/user");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(token);
 
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
